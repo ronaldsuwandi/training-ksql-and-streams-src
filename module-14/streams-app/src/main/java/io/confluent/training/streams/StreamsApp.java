7927e2a5d8d2137886a1096f92ea37574e797577
Gabriel N Schenker <gnschenker@gmail.com>
2018-09-03 09:51:28 +0200

    Replaced old versions of Kafka and Confluent components with newest ones

--- module-14/streams-app/src/main/java/io/confluent/training/streams/StreamsApp.java
+++ module-14/streams-app/src/main/java/io/confluent/training/streams/StreamsApp.java
@@ -15,7 +15,7 @@ import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
-import org.apache.kafka.streams.Consumed;
+import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.Produced;
 import org.apache.kafka.streams.kstream.TimeWindows;
 import org.apache.kafka.streams.kstream.Windowed;
