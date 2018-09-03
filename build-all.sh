1df752a17f96d25bc202a8a28d80900fb0d41497
Gabriel N Schenker <gnschenker@gmail.com>
2018-09-03 16:28:03 +0300

    Using frekele/gradle image to build Gradle based projects. Fixed streams-app in Module 14

---
+++ build-all.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+LABS=$(pwd)
+FILES=$(find `pwd` -name build.gradle)
+for FILE in $FILES
+do
+    DIR=$(dirname $FILE)
+    cd $DIR
+    echo "*** Compiling project in $DIR"
+    docker container run --rm \
+        -v "$PWD":/home/gradle/project \
+        -v "$HOME"/.gradle:/root/.gradle \
+        -w /home/gradle/project \
+        frekele/gradle:latest gradle build --warning-mode=none
+done
