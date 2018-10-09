d7345b786d04ce8b8f922ed6f07e40e635e19dee
Gabriel N Schenker <gnschenker@gmail.com>
2018-10-09 11:26:04 +0300

    New structure module 10.02

---
+++ labs/m-10/join-sample/src/main/java/streams/JoinSample.java
@@ -0,0 +1,62 @@
+package streams;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Produced;
+import org.apache.kafka.streams.kstream.JoinWindows;
+import org.apache.kafka.streams.kstream.Joined;
+
+public class JoinSample {
+    final static String APPLICATION_ID = "join-sample-v0.1.0";
+    final static String APPLICATION_NAME = "Join Sample";
+
+    public static void main(String[] args) {
+        System.out.printf("*** Starting %s Application ***%n", APPLICATION_NAME);
+
+        Properties config = getConfig();
+        Topology topology = getTopology();
+        KafkaStreams streams =  startApp(config, topology);
+
+        setupShutdownHook(streams);
+    }
+
+    private static Topology getTopology(){
+        StreamsBuilder builder = new StreamsBuilder();
+
+        final Serde<String> stringSerde = Serdes.String();
+
+        // TODO: here we construct the Kafka Streams topology
+    }
+
+    private static Properties getConfig(){
+        Properties settings = new Properties();
+        settings.put(StreamsConfig.APPLICATION_ID_CONFIG, APPLICATION_ID);
+        settings.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
+        return settings;        
+    }
+
+    private static KafkaStreams startApp(Properties config, Topology topology){
+        KafkaStreams streams = new KafkaStreams(topology, config);
+        streams.start();
+        return streams;
+    }
+
+    private static void setupShutdownHook(KafkaStreams streams){
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.printf("### Stopping %s Application ###%n", APPLICATION_NAME);
+            streams.close();
+        }));
+    }
+}
