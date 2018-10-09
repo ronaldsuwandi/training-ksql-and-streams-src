18eda9372f231ae07d4c345efa42442eebb56e9c
Gabriel N Schenker <gnschenker@gmail.com>
2018-10-09 09:15:49 +0300

    New structure module 10.01

--- labs/m-10/json-sample/src/main/java/streams/JsonSample.java
+++ labs/m-10/json-sample/src/main/java/streams/JsonSample.java
@@ -3,29 +3,35 @@ package streams;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
-import java.util.concurrent.TimeUnit;
 
+import org.apache.kafka.common.serialization.Serializer;
+import org.apache.kafka.common.serialization.Deserializer;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.KafkaStreams;
-import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.Topology;
-import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.Produced;
-import org.apache.kafka.streams.kstream.JoinWindows;
-import org.apache.kafka.streams.kstream.Joined;
+import io.confluent.kafka.serializers.KafkaJsonSerializer;
+import io.confluent.kafka.serializers.KafkaJsonDeserializer;
 
-public class JoinSample {
-    final static String APPLICATION_ID = "join-sample-v0.1.0";
-    final static String APPLICATION_NAME = "Join Sample";
+public class JsonSample {
+    final static String APPLICATION_ID = "json-sample-v0.1.0";
+    final static String APPLICATION_NAME = "JSON Sample";
+
+    // POJO class
+    static public class TempReading {
+        public String station;
+        public Double temperature;
+        public Long timestamp;
+    }
 
     public static void main(String[] args) {
         System.out.printf("*** Starting %s Application ***%n", APPLICATION_NAME);
 
-        StreamsConfig config = getConfig();
+        Properties config = getConfig();
         Topology topology = getTopology();
         KafkaStreams streams =  startApp(config, topology);
 
@@ -36,22 +42,23 @@ public class JoinSample {
         StreamsBuilder builder = new StreamsBuilder();
 
         final Serde<String> stringSerde = Serdes.String();
+        final Serde<TempReading> temperatureSerde = getJsonSerde();
 
         // TODO: here we construct the Kafka Streams topology
-
-        Topology topology = builder.build();
-        return topology;
     }
 
-    private static StreamsConfig getConfig(){
+    private static Serde<TempReading> getJsonSerde(){
+        // TODO: create the JSON serde
+    }
+
+    private static Properties getConfig(){
         Properties settings = new Properties();
         settings.put(StreamsConfig.APPLICATION_ID_CONFIG, APPLICATION_ID);
         settings.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
-        StreamsConfig config = new StreamsConfig(settings);
-        return config;        
+        return settings;        
     }
 
-    private static KafkaStreams startApp(StreamsConfig config, Topology topology){
+    private static KafkaStreams startApp(Properties config, Topology topology){
         KafkaStreams streams = new KafkaStreams(topology, config);
         streams.start();
         return streams;
