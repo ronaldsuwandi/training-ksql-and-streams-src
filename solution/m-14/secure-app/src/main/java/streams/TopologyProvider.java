b831b3697f69b68322e23f3641276ff4ebb5e976
Chuck <chuck@confluent.io>
2019-08-15 12:28:10 -0700

    updating labs to 5.3/2.3

--- solution/m-14/secure-app/src/main/java/streams/TopologyProvider.java
+++ solution/m-14/secure-app/src/main/java/streams/TopologyProvider.java
@@ -3,8 +3,6 @@ package streams;
 import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.StreamsBuilder;
 
-import java.util.Properties;
-
 public class TopologyProvider {
     public Topology getTopology() {
         final StreamsBuilder builder = new StreamsBuilder();
