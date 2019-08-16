012795ad0ebe63a5f4dc12b9cd4aa6883d1631b3
Chuck <chuck@confluent.io>
2019-08-16 15:49:41 -0700

    refactor and document python program

--- solution/m-08/python/main.py
+++ solution/m-08/python/main.py
@@ -2,6 +2,21 @@ from confluent_kafka import Producer, Consumer, KafkaError
 import requests
 import time
 
+
+def main():
+    """
+    This program produces quotes to a Kafka Topic, then
+    makes a REST API call to create a KSQL query,
+    and finally consumes the transformed messages created by
+    the query.
+    """
+    print(">>> Starting Python Kafka Client...")
+    produce_quotes()
+    call_ksql()
+    consume_lowercase_quotes()
+    print("<<< Ending Python Kafka Client...")
+
+# Initialization
 quotes = [
     "Kafka enables the Confluent Streaming Platform",
     "Confluent offers a Streaming Platform powered by Kafka",
@@ -13,17 +28,29 @@ output_topic = "quotes"
 input_topic = "QUOTES_LOWER"
 bootstrap_servers = "kafka:9092"
 
+# Function Definitions
 
 def produce_quotes():
     print("------ Writing quotes to topic '" + output_topic + "' ------")
     producer = Producer({"bootstrap.servers": bootstrap_servers})
-    for x in range(0, len(quotes)):
-        quote = quotes[x]
+    for quote in quotes:
         print("*** writing: " + quote)
         producer.produce(output_topic, value=quote)
     producer.flush()
     print("------ done writing quotes ------")
 
+
+def call_ksql():
+    print("--------- Posting to KSQL Server ---------")
+    ksql = "CREATE STREAM quotes_orig (line STRING) WITH(KAFKA_TOPIC='quotes', VALUE_FORMAT='DELIMITED');"
+    post_expression(ksql)
+
+    # wait until KSQL server has created the stream
+    time.sleep(2)
+
+    ksql = "CREATE STREAM quotes_lower AS SELECT LCASE(line) FROM quotes_orig;"
+    post_expression(ksql)
+    print("--------- done posting to KSQL Server -----------")
 
 def post_expression(ksql):
     headers = {
@@ -40,20 +67,6 @@ def post_expression(ksql):
         print("ERROR: " + str(r.status_code) + ", " + r.text)
         raise
     print(str(r.status_code) + ", " + r.text)
-
-
-def call_ksql():
-    print("--------- Posting to KSQL Server ---------")
-    ksql = "CREATE STREAM quotes_orig (line STRING) WITH(KAFKA_TOPIC='quotes', VALUE_FORMAT='DELIMITED');"
-    post_expression(ksql)
-
-    # wait until KSQL server has created the stream
-    time.sleep(2)
-
-    ksql = "CREATE STREAM quotes_lower AS SELECT LCASE(line) FROM quotes_orig;"
-    post_expression(ksql)
-    print("--------- done posting to KSQL Server -----------")
-
 
 def consume_lowercase_quotes():
     print("------ Reading from topic '" + input_topic + "' ------")
@@ -83,9 +96,6 @@ def consume_lowercase_quotes():
     consumer.close()
 
 
+# Call main function if program is run from command line
 if __name__ == "__main__":
-    print(">>> Starting Python Kafka Client...")
-    produce_quotes()
-    call_ksql()
-    consume_lowercase_quotes()
-    print("<<< Ending Python Kafka Client...")
+    main()
