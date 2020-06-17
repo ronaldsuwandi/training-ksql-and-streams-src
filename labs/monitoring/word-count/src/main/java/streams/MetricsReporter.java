7dd01f9d5ef8fb379f7565ebbcc68c5c1b889d44
dhbrenner <donald@confluent.io>
2020-06-17 17:42:07 -0500

    vscode, gradle wrapper and gitignore changes
    vscode, gradle wrapper and gitignore changes

---
+++ labs/monitoring/word-count/src/main/java/streams/MetricsReporter.java
@@ -0,0 +1,20 @@
+package streams;
+
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import org.apache.kafka.common.MetricName;
+import org.apache.kafka.common.Metric;
+import org.apache.kafka.streams.KafkaStreams;
+
+public class MetricsReporter {
+    public void reportMetrics(KafkaStreams streams) throws InterruptedException {
+        while(true){
+            System.out.println("--- Application Metrics ---");
+            Map<MetricName, ? extends Metric> metrics = streams.metrics();
+            for (Map.Entry<MetricName, ? extends Metric> metric: metrics.entrySet()){
+                System.out.println(metric.getKey().toString() + ", " + metric.getValue().metricValue());
+            }
+            TimeUnit.SECONDS.sleep(10);
+        }
+    }
+}
